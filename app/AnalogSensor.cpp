869da9c2785839677a833d63ddcdb6932a091df9
smahajan07 <mahajan.sarthak28@gmail.com>
2018-10-02 22:00:21 -0400

    identified bug 2 and fixed

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -13,7 +13,7 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+    std::shared_ptr<std::vector<int> > readings (new std::vector<int>(mSamples, 10));
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
